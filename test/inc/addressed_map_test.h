dfb81a4e2c5238effa5d2bff4cec0d1a8fd2e324
Hoblirm <hoblirm@gmail.com>
2014-03-09 16:17:27 -0400

    Set up addressed map to be usable by a class.

--- test/inc/addressed_map_test.h
+++ test/inc/addressed_map_test.h
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+class Food {
+  public:
+    int id;
+    string name;
+    Food () : id(-1), name("") {}
+    Food (int i, string str) : id(i), name(str) {}
+    int toInt() const {return id;}
+};
+
+
 class addressed_map_test : public CxxTest::TestSuite {
   public:
 
@@ -24,6 +34,28 @@ class addressed_map_test : public CxxTest::TestSuite {
       TS_ASSERT(addr.find(0)->second == "Apple");
       TS_ASSERT(addr.find(2)->second == "Carrot");
     }
+
+    // It should be able to perform a simple insert and find on a class.
+    void testZeroBasedKeyOnClass(void) {
+      addressed_map<Food,string> addr;
+
+      Food f1(0,"Apple");
+      Food f2(1,"Banana");
+      Food f3(2,"Carrot");
+
+      string str1("Isle 1");
+      string str2("Isle 2");
+      string str3("Isle 3");
+
+      addr.insert(pair<Food,string>(f1,str1));
+      addr.insert(pair<Food,string>(f2,str2));
+      addr.insert(pair<Food,string>(f3,str3));
+
+      TS_ASSERT(addr.find(f1)->second == "Isle 1");
+      TS_ASSERT(addr.find(f2)->second == "Isle 2");
+      TS_ASSERT(addr.find(f3)->second == "Isle 3");
+    }
+
 
     // It should be able to perform inserts on negative keys.
     void testNegativeKey(void) {
@@ -93,6 +125,28 @@ class addressed_map_test : public CxxTest::TestSuite {
       TS_ASSERT(addr[2] == "Carrot");
     }
 
+    // It should be able to used the [] operator for find and insert on a class
+    void testBraceOperatorOnClass(void) {
+      addressed_map<Food,string> addr;
+
+      Food f1(0, "Apple");
+      Food f2(1, "Banana");
+      Food f3(2, "Carrot");
+
+      string str1("Isle 1");
+      string str2("Isle 2");
+      string str3("Isle 3");
+
+      addr[f1] = str1;
+      addr[f2] = str2;
+      addr[f3] = str3;
+
+      TS_ASSERT(addr.size() == 3);
+      TS_ASSERT(addr[f2] == "Isle 2");
+      TS_ASSERT(addr[f1] == "Isle 1");
+      TS_ASSERT(addr[f3] == "Isle 3");
+    }
+
     // It should be able to remove entries by key.
     void testKeyRemoval(void) {
       addressed_map<int,string> addr;
@@ -123,6 +177,40 @@ class addressed_map_test : public CxxTest::TestSuite {
       TS_ASSERT(addr.find(4) == addr.end());
     }
 
+    // It should be able to remove entries by key on a class.
+    void testKeyRemovalOnClass(void) {
+      addressed_map<Food,string> addr;
+
+      Food f1(0, "Apple");
+      Food f2(1, "Banana");
+      Food f3(2, "Carrot");
+
+      string str1("Isle 1");
+      string str2("Isle 2");
+      string str3("Isle 3");
+
+      addr.insert(pair<Food,string>(f1,str1));
+      addr.insert(pair<Food,string>(f2,str2));
+      addr.insert(pair<Food,string>(f3,str3));
+
+      // Verify everything was properly inserted.
+      TS_ASSERT(addr.find(f1)->second == "Isle 1");
+      TS_ASSERT(addr.find(f2)->second == "Isle 2");
+      TS_ASSERT(addr.find(f3)->second == "Isle 3");
+      TS_ASSERT_EQUALS(addr.size(),3);
+
+      // Remove the middle entry.
+      addr.erase(f2);
+
+      // Ensure that the middle entry was removed.
+      TS_ASSERT_EQUALS(addr.size(),2);
+      TS_ASSERT(addr.find(Food(-1,"Apple")) == addr.end());
+      TS_ASSERT_EQUALS(addr.find(f1)->second, "Isle 1");
+      TS_ASSERT(addr.find(Food(1,"Apple")) == addr.end());
+      TS_ASSERT_EQUALS(addr.find(f3)->second, "Isle 3");
+      TS_ASSERT(addr.find(Food(4,"Apple")) == addr.end());
+    }
+
     // It should be able to provide the count for a key.
     void testKeyCount(void) {
       addressed_map<int,string> addr;
@@ -140,6 +228,29 @@ class addressed_map_test : public CxxTest::TestSuite {
       TS_ASSERT_EQUALS(addr.count(2),1);
       TS_ASSERT_EQUALS(addr.count(-1),0);
       TS_ASSERT_EQUALS(addr.count(3),0);
+    }
+
+    // It should be able to provide the count for a key on a class.
+    void testKeyCountOnClass(void) {
+      addressed_map<Food,string> addr;
+      
+      Food f1(0, "Apple");
+      Food f2(1, "Banana");
+      Food f3(2, "Carrot");
+
+      string str1("Apple");
+      string str2("Banana");
+      string str3("Carrot");
+
+      addr.insert(pair<Food,string>(f1,str1));
+      addr.insert(pair<Food,string>(f2,str2));
+      addr.insert(pair<Food,string>(f3,str3));
+
+      TS_ASSERT_EQUALS(addr.count(f1),1);
+      TS_ASSERT_EQUALS(addr.count(f2),1);
+      TS_ASSERT_EQUALS(addr.count(f3),1);
+      TS_ASSERT_EQUALS(addr.count(Food(-1,"Apple")),0);
+      TS_ASSERT_EQUALS(addr.count(Food(3,"Apple")),0);
     }
 
     // It should be able to iterate all the objects in the addr.
@@ -246,4 +357,51 @@ class addressed_map_test : public CxxTest::TestSuite {
       }
       TS_ASSERT(addr.find(300001) == addr.end());
     }
+
+    // It should be able to perform inserts with many entries on a class.
+    void testStressKeyOnClass(void) {
+      addressed_map<Food,int> addr;
+
+      // Insert many entries into the map.
+      for (int key=-100000;key<key-1000; key++) {
+        addr.insert(pair<Food,int>(Food(key,"Berry"),key*10));
+      }
+      for (int key=10000; key < 300000; key++) {
+        addr.insert(pair<Food,int>(Food(key,"Berry"),key*10));
+      }
+      for (int key=-20;key<100;key++) {
+        addr.insert(pair<Food,int>(Food(key,"Berry"),key*10));
+      }
+      for (int key=-300000;key>-400000;key--) {
+        addr.insert(pair<Food,int>(Food(key,"Berry"),key*10));
+      }
+
+      // Verify that all the inserted keys can be found.
+      for (int key=-100000;key<key-1000; key++) {
+        TS_ASSERT_EQUALS(addr.find(Food(key,"Berry"))->second,key*10);
+      }
+      for (int key=10000; key < 300000; key++) {
+        TS_ASSERT_EQUALS(addr.find(Food(key,"Berry"))->second,key*10);
+      }
+      for (int key=-20;key<100;key++) {
+        TS_ASSERT_EQUALS(addr.find(Food(key,"Berry"))->second,key*10);
+      }
+      for (int key=-300000;key>-400000;key--) {
+        TS_ASSERT_EQUALS(addr.find(Food(key,"Berry"))->second,key*10);
+      }
+
+      // Verify that all the unused keys return 0
+      TS_ASSERT(addr.find(Food(-400001,"Berry")) == addr.end());
+      for (int key=-299999;key < -100000; key++) {
+        TS_ASSERT(addr.find(Food(key,"Berry")) == addr.end());
+      }
+      for (int key=-999;key < -20; key++) {
+        TS_ASSERT(addr.find(Food(key,"Berry")) == addr.end());
+      }
+      for (int key=101; key < 10000; key++) {
+        TS_ASSERT(addr.find(Food(key,"Berry")) == addr.end());
+      }
+      TS_ASSERT(addr.find(Food(300001,"Berry")) == addr.end());
+    }
+
 };
