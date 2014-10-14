edc1110e6d3c02eb771ad6be3d614e2604792e93
Hoblirm <hoblirm@gmail.com>
2014-10-14 06:23:36 -0400

    Added fnv hash.

---
+++ inc/fnv_1a_hash.h
@@ -0,0 +1,152 @@
+#include <stdlib.h>
+
+/**
+ * Global functionallity.  This is functionality that is shared by
+ * ALL templates.  Even those that are specialized.
+ **/
+class FNV1aHash_base
+{
+  protected:
+  static std::size_t offset_basis;
+  static std::size_t FNV_prime;
+  static bool initialized;
+
+  public:
+  FNV1aHash_base(){
+    if (!initialized)
+    {
+      initialized = true;
+      switch (sizeof(std::size_t))
+      {
+        default:
+          // For 32 bit machines:
+          FNV_prime = 16777619u;
+          offset_basis = 2166136261u;
+          break;
+
+        case 8:
+          // For 64 bit machines:
+          printf("We are on a 64 bit machine~\n");
+          FNV_prime = 1099511628211u;
+          offset_basis = 14695981039346656037u;
+          break;
+
+        case 16:
+          // For 128 bit machines:
+          FNV_prime = 309485009821345068724781401u;
+          offset_basis = 275519064689413815358837431229664493455u;
+          break;
+
+        case 32:
+          // For 256 bit machines:
+          FNV_prime = 374144419156711147060143317175368453031918731002211u;
+          offset_basis = 100029257958052580907070968620625704837092796014241193945225284501741471925557u;
+          break;
+      }
+    }
+  };
+
+/*  uint64_t hash64(uint64_t key) const
+  {
+    uint8_t* bytes = (uint8_t*)(&key);
+    uint32_t hash = 2166136261U;
+    hash = (16777619U * hash) ^ bytes[0];
+    hash = (16777619U * hash) ^ bytes[1];
+    hash = (16777619U * hash) ^ bytes[2];
+    hash = (16777619U * hash) ^ bytes[3];
+    hash = (16777619U * hash) ^ bytes[4];
+    hash = (16777619U * hash) ^ bytes[5];
+    hash = (16777619U * hash) ^ bytes[6];
+    hash = (16777619U * hash) ^ bytes[7];
+    return hash;
+  }*/
+
+};
+
+bool FNV1aHash_base::initialized = false;
+std::size_t FNV1aHash_base::FNV_prime = 0;
+std::size_t FNV1aHash_base::offset_basis = 0;
+
+/*
+ * Default functionality for non-pointers.
+ * */
+template <class Key> class FNV1aHash : FNV1aHash_base
+{
+  int mNumberOfBytes;
+
+  public:
+  FNV1aHash(){
+    if (!FNV1aHash_base::initialized) mNumberOfBytes = sizeof(Key);
+  };
+
+  std::size_t operator()(Key&) const;
+};
+
+template <class Key> std::size_t FNV1aHash<Key>::operator()(Key& key) const
+{
+  const unsigned char * data = (const unsigned char *)&key;
+  size_t hash = FNV1aHash_base::offset_basis;
+  int len = mNumberOfBytes;
+  unsigned int c;
+  while (len--){
+    c = *data;
+    hash = (hash ^ c) * FNV1aHash_base::FNV_prime;
+    ++data;
+  }
+  return hash;
+}
+
+/*
+ * Default functionality for ALL pointer types.
+ **/
+
+/*
+template <class Key> class FNV1aHash<Key*> : FNV1aHash_base
+{
+  int mNumberOfBytes;
+
+  public:
+  FNV1aHash(){
+    if (!FNV1aHash_base::initialized) mNumberOfBytes = sizeof(Key);
+  };
+
+  std::size_t operator()(const Key*) const;
+};
+
+template <class Key> std::size_t FNV1aHash<Key*>::operator()(const Key* key) const
+{
+  const unsigned char * data = (const unsigned char *)key;
+  size_t hash = FNV1aHash_base::offset_basis;
+  int len = mNumberOfBytes;
+  unsigned int c;
+  while (len--){
+    c = *data;
+    hash = (hash ^ c) * FNV1aHash_base::FNV_prime;
+    ++data;
+  }
+  return hash;
+}
+*/
+
+/*
+ * Specialized functionality for const char*
+ * */
+
+
+   template <> class FNV1aHash<const char*> : FNV1aHash_base
+   {
+     public:
+       FNV1aHash(){}
+       std::size_t operator()(const char* str) const;
+   }; 
+
+   std::size_t FNV1aHash<const char*>::operator()(const char* str) const
+   {
+   size_t hash = offset_basis;
+   unsigned int c;
+   while (c = *str++){
+   hash = (hash ^ c) * FNV_prime;
+   }
+   return hash;
+   }
+   
--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <sys/time.h>
 #include <stdio.h>
-#include <addressed_map>
-#include <object_pool.h>
-
 #include <sstream>
 #include <iostream>
+#include <cstddef>
+#include <string.h>
+#include <tr1/unordered_map>
+#include <tr1/functional>
 
+#include <fnv_1a_hash.h>
 
 class Foo {
   public:
@@ -18,16 +20,68 @@ class Foo {
   ~Foo(void){}
 };
 
+struct CharCmpFunctor{
+  bool operator()(const char* str1, const char* str2) const{
+    return !strcmp(str1,str2);
+  }
+};
 
 int main(int argc, char** argv) {
-  object_pool<Foo> pool(1);
-  try{
-  pool.allocate();
-  pool.allocate();
+  //Test unordered_map
+  std::tr1::unordered_map<const char*, Foo> m;
+  Foo f;
+  f.id = 1337;
+  f.name = "One";
+  m.insert(std::pair<const char*,Foo>(f.name.c_str(),f));
+  if (m.count("One"))
+    printf("Unordered_map hashes char*\n");
+  else
+    printf("Unordered_map does NOT hash char*\n");
+
+  std::tr1::unordered_map<const char*, Foo, FNV1aHash<const char*>, CharCmpFunctor> n;
+  n.insert(std::pair<const char*,Foo>(f.name.c_str(),f));
+  if (n.count("One"))
+    printf("Unordered_map hashes char*\n");
+  else
+    printf("Unordered_map does NOT hash char*\n");
+
+
+  if (!(-1)){
+    printf("!-1 is true\n");
+  }else{
+    printf("!-1 is f\n");
   }
-  catch(exception& e){
-    printf("We caught that pesky exception: %s\n",e.what());
+  if (!(2)){
+         printf("!2 is true\n");
+       }else{
+           printf("!2 is f\n");
+         }
+
+  //Test hash
+  char nts1[] = "Test";
+  char nts2[] = "Test";
+  const char* tmp = (const char*)nts2;
+  while (*tmp)
+  {
+    char t[16];
+    t[0] = tmp[0];
+    t[1] = '\n';
+    t[2] = '\0';
+    printf(t);
+    ++tmp;
   }
+
+  std::string str1 (nts1);
+  std::string str2 (nts2);
+
+  printf("size:%d\n",sizeof(size_t));
+  std::tr1::hash<char*> ptr_hash;
+  std::tr1::hash<std::string> str_hash;
+
+  std::cout << "same hashes:\n" << std::boolalpha;
+  std::cout << "nts1 and nts2: " << (ptr_hash(nts1)==ptr_hash(nts2)) << '\n';
+  std::cout << "str1 and str2: " << (str_hash(str1)==str_hash(str2)) << '\n';
+
   return 0;
 }
 
